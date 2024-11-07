16ae02529bb6319494a88126e0a439983c82ca34
Wendley Santos <wendley.sdsilva@gmail.com>
2024-11-07 19:14:21 -0300

    user post

---
+++ src/main/resources/static/js/postUser.js
@@ -0,0 +1,16 @@
+let form = document.getElementyById("form");
+
+let nome = document.getElementyById("txtNome");
+let cpf = document.getElementyById("txtCpf");
+let email = document.getElementyById("txtEmail");
+let telefone = document.getElementyById("Telefone");
+let login = document.getElementyById("txtUsuario");
+let senha = document.getElementyById("txtSenha");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    
+    
+});
+
